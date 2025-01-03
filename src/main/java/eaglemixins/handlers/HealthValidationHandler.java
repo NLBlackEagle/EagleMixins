859da1e5a3a2b87f6160b1941f0472aebd8b85e1
fonnymunkey <fonnymunkey@gmail.com>
2025-01-02 20:21:04 -0500

    update health checks

--- src/main/java/eaglemixins/handlers/HealthValidationHandler.java
+++ src/main/java/eaglemixins/handlers/HealthValidationHandler.java
@@ -2,33 +2,56 @@ package eaglemixins.handlers;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
-import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class HealthValidationHandler {
-
-    @SubscribeEvent
-    public void onLivingHurt(LivingHurtEvent event) {
-        validateHealth(event);
-        if (Float.isNaN(event.getAmount()) || event.getAmount() == Float.NEGATIVE_INFINITY) {
-            event.setCanceled(true);
+    
+    @SubscribeEvent(priority = EventPriority.HIGHEST, receiveCanceled = true)
+    public static void onLivingHurtHighest(LivingHurtEvent event) {
+        if(event.getEntityLiving() == null) return;
+        if(event.getEntityLiving().world.isRemote) return;
+        
+        validateHealth(event.getEntityLiving());
+        
+        if(Float.isNaN(event.getAmount()) || event.getAmount() == Float.NEGATIVE_INFINITY) {
+            event.setAmount(0.0F);
         }
     }
-
-    @SubscribeEvent
-    public void onLivingDeath(LivingDeathEvent event) {
-        validateHealth(event);
+    
+    @SubscribeEvent(priority = EventPriority.LOWEST, receiveCanceled = true)
+    public static void onLivingHurtLowest(LivingHurtEvent event) {
+        if(event.getEntityLiving() == null) return;
+        if(event.getEntityLiving().world.isRemote) return;
+        
+        validateHealth(event.getEntityLiving());
+        
+        if(Float.isNaN(event.getAmount()) || event.getAmount() == Float.NEGATIVE_INFINITY) {
+            event.setAmount(0.0F);
+        }
+    }
+    
+    @SubscribeEvent(priority = EventPriority.HIGHEST, receiveCanceled = true)
+    public static void onLivingDeathHighest(LivingDeathEvent event) {
+        if(event.getEntityLiving() == null) return;
+        if(event.getEntityLiving().world.isRemote) return;
+        
+        validateHealth(event.getEntityLiving());
+    }
+    
+    @SubscribeEvent(priority = EventPriority.LOWEST, receiveCanceled = true)
+    public static void onLivingDeathLowest(LivingDeathEvent event) {
+        if(event.getEntityLiving() == null) return;
+        if(event.getEntityLiving().world.isRemote) return;
+        
+        validateHealth(event.getEntityLiving());
     }
 
-    private void validateHealth(LivingEvent event) {
-        EntityLivingBase entity = event.getEntityLiving();
-        if (entity.world.isRemote) {
-            return;
-        }
+    private static void validateHealth(EntityLivingBase entity) {
         float health = entity.getHealth();
-        if (Float.isNaN(health) || Float.isInfinite(health) || health < 0F) {
-            entity.setHealth(0F);
+        if(Float.isNaN(health) || Float.isInfinite(health) || health < 0.0F) {
+            entity.setHealth(0.0F);
         }
     }
 }
