1e75df6aefdd861dbdf5dba0b469d176c20ec2a8
Nischhelm <nischhelm@gmail.com>
2025-01-26 20:36:26 +0100

    bump version 1.0.5

--- src/main/java/eaglemixins/EagleMixins.java
+++ src/main/java/eaglemixins/EagleMixins.java
@@ -11,7 +11,7 @@ import org.apache.logging.log4j.Logger;
 public class EagleMixins {
 
     public static final String MODID = "eaglemixins";
-    public static final String VERSION = "1.0.4";
+    public static final String VERSION = "1.0.5";
     public static final String NAME = "EagleMixins";
     public static final Logger LOGGER = LogManager.getLogger(NAME);
 
