3d780cb9da856a597c4930408888a25cefba5d93
Nischhelm <nischhelm@gmail.com>
2025-02-05 20:00:05 +0100

    v1.0.9 - quite a big jump there

--- src/main/java/eaglemixins/EagleMixins.java
+++ src/main/java/eaglemixins/EagleMixins.java
@@ -11,7 +11,7 @@ import org.apache.logging.log4j.Logger;
 public class EagleMixins {
 
     public static final String MODID = "eaglemixins";
-    public static final String VERSION = "1.0.6";
+    public static final String VERSION = "1.0.9";
     public static final String NAME = "EagleMixins";
     public static final Logger LOGGER = LogManager.getLogger(NAME);
 
