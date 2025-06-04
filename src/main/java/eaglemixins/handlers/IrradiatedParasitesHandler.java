eec34d71e6b189b0c2de03e151ed9ec793b8592d
Nischhelm <nischhelm@gmail.com>
2025-06-04 22:52:03 +0200

    fixed irradiated parasites
    - changed radiation source to be entity rads percent
    - moved check to entity specific timing to smear it out some more over the ticks
    - added random uuids

--- src/main/java/eaglemixins/handlers/IrradiatedParasitesHandler.java
+++ src/main/java/eaglemixins/handlers/IrradiatedParasitesHandler.java
@@ -2,7 +2,7 @@ package eaglemixins.handlers;
 
 import com.dhanantry.scapeandrunparasites.entity.ai.misc.EntityParasiteBase;
 import eaglemixins.config.ForgeConfigHandler;
-import nc.capability.radiation.source.IRadiationSource;
+import nc.capability.radiation.entity.IEntityRads;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
@@ -13,9 +13,9 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import java.util.UUID;
 
 public class IrradiatedParasitesHandler {
-    private static final UUID HP_UUID = UUID.fromString("todo");
-    private static final UUID DMG_UUID = UUID.fromString("todo");
-    private static final UUID ARMOR_UUID = UUID.fromString("todo");
+    private static final UUID HP_UUID = UUID.fromString("88b143a9-2850-4415-9dfc-810159c5179f");
+    private static final UUID DMG_UUID = UUID.fromString("e6c9d15c-7835-4eb0-ad7e-a41c90a5a483");
+    private static final UUID ARMOR_UUID = UUID.fromString("0cdaa41f-92d6-4575-9acc-3527810a859e");
 
     @SubscribeEvent
     private static void onEntityTick(LivingEvent.LivingUpdateEvent event){
@@ -23,23 +23,19 @@ public class IrradiatedParasitesHandler {
         if(!(entity instanceof EntityParasiteBase)) return;
         World world = entity.world;
         if(world.isRemote) return;
-        if(world.getTotalWorldTime() % 100 != 57) return;
+        if(entity.ticksExisted % 100 != 57) return;
 
-        IRadiationSource chunkRadiationCap = world.getChunk(entity.getPosition()).getCapability(IRadiationSource.CAPABILITY_RADIATION_SOURCE, null);
-        if(chunkRadiationCap == null) return;
-        double radiationLevel = chunkRadiationCap.getRadiationLevel();
-
-        /*IEntityRads entityRadiation = entity.getCapability(IEntityRads.CAPABILITY_ENTITY_RADS, null);
+        IEntityRads entityRadiation = entity.getCapability(IEntityRads.CAPABILITY_ENTITY_RADS, null);
         if(entityRadiation == null) return;
-        double rads = entityRadiation.getRadsPercentage();*/ //TODO: use this instead of the above chunk code if the entity rads is supposed to be used instead of the chunk radiation level
+        double radsPercent = entityRadiation.getRadsPercentage();
 
         entity.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).removeModifier(HP_UUID);
         entity.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).removeModifier(DMG_UUID);
         entity.getEntityAttribute(SharedMonsterAttributes.ARMOR).removeModifier(ARMOR_UUID);
 
-        double hpMod = - Math.min(radiationLevel * ForgeConfigHandler.irradiated.hpMultiplier, ForgeConfigHandler.irradiated.hpUpperLimit);
-        double dmgMod = - Math.min(radiationLevel * ForgeConfigHandler.irradiated.dmgMultiplier, ForgeConfigHandler.irradiated.dmgUpperLimit);
-        double armorMod = - Math.min(radiationLevel * ForgeConfigHandler.irradiated.armorMultiplier, ForgeConfigHandler.irradiated.armorUpperLimit);
+        double hpMod = - Math.min(radsPercent * ForgeConfigHandler.irradiated.hpMultiplier, ForgeConfigHandler.irradiated.hpUpperLimit);
+        double dmgMod = - Math.min(radsPercent * ForgeConfigHandler.irradiated.dmgMultiplier, ForgeConfigHandler.irradiated.dmgUpperLimit);
+        double armorMod = - Math.min(radsPercent * ForgeConfigHandler.irradiated.armorMultiplier, ForgeConfigHandler.irradiated.armorUpperLimit);
 
         entity.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).applyModifier(new AttributeModifier(HP_UUID, "irradiated hp", hpMod, 2));
         entity.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).applyModifier(new AttributeModifier(DMG_UUID, "irradiated dmg", dmgMod, 2));
