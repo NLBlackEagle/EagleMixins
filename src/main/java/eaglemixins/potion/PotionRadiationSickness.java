e297d37437d99accb391bda51b6145e53e66907e
Vincent <nlblackeagle@live.nl>
2025-06-10 05:38:07 +0200

    Properly fixed break; for case

--- src/main/java/eaglemixins/potion/PotionRadiationSickness.java
+++ src/main/java/eaglemixins/potion/PotionRadiationSickness.java
@@ -41,16 +41,17 @@ public class PotionRadiationSickness extends PotionBase {
 
                     if (currentHealth > 0.75f * maxHealth) {
                         entityLivingBase.attackEntityFrom(RadiationDamageSource.RADIATION, 1.0F);
-                        break;
+
                     }
                 }
+                break;
             case 4:
                 entityLivingBase.addPotionEffect(new PotionEffect(PotionRadiationWeakness.INSTANCE));
                 entityLivingBase.addPotionEffect(new PotionEffect(PotionRadiationFatigue.INSTANCE));
                 if (entityLivingBase instanceof EntityPlayer) {
                     entityLivingBase.attackEntityFrom(RadiationDamageSource.RADIATION, 1.0F);
-                    break; //entityLivingBase.attackEntityFrom(new DamageSource("Radiation"), 1.0F);
                 }
+                break;
         }
     }
 }
