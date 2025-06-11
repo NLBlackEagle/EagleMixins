fe7daffc3e00bfc30597fbf69bd0da6edd60e02a
Vincent <nlblackeagle@live.nl>
2025-06-11 20:49:11 +0200

    Versioning

--- src/main/java/eaglemixins/EagleMixins.java
+++ src/main/java/eaglemixins/EagleMixins.java
@@ -13,7 +13,7 @@ import org.apache.logging.log4j.Logger;
 public class EagleMixins {
 
     public static final String MODID = "eaglemixins";
-    public static final String VERSION = "1.0.9";
+    public static final String VERSION = "1.1.0";
     public static final String NAME = "EagleMixins";
     public static final Logger LOGGER = LogManager.getLogger(NAME);
 
