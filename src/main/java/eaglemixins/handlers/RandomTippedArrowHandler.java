c14684e55c9b5801218f4a07e2b224250a0399c1
Nischhelm <nischhelm@gmail.com>
2025-07-05 11:33:52 +0200

    fixed random tipped arrows
    - player was able to add effects on entities just by having tipped arrows in hand
    - instant effects applied for a LONG time
    - effects always applied with effect lvl II
    - why didnt we use vanilla handling in the first place
    - in theory a tipped arrow skelly could also apply its arrow effects using thorns or whatever other side dmg bc there was no check for using an arrow

--- src/main/java/eaglemixins/handlers/RandomTippedArrowHandler.java
+++ src/main/java/eaglemixins/handlers/RandomTippedArrowHandler.java
@@ -3,6 +3,9 @@ package eaglemixins.handlers;
 import eaglemixins.config.ForgeConfigHandler;
 import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.entity.projectile.EntityTippedArrow;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemStack;
@@ -60,20 +63,26 @@ public class RandomTippedArrowHandler {
 
         if (event.getSource() == null || event.getSource().getTrueSource() == null) return;
         if (!(event.getSource().getTrueSource() instanceof EntityLivingBase)) return;
+        if (!(event.getSource().getImmediateSource() instanceof EntityArrow)) return;
+
         EntityLivingBase attacker = (EntityLivingBase) event.getSource().getTrueSource();
+        if(attacker instanceof EntityPlayer) return; //:skull:
 
         ItemStack mainhand = attacker.getHeldItemMainhand();
         if (!(mainhand.getItem() instanceof ItemBow)) return;
         ItemStack offhand = attacker.getHeldItemOffhand();
         if(!(offhand.getItem() instanceof ItemTippedArrow)) return;
 
-        Random random = new Random();
-        int maxDuration = random.nextInt(1200) + 200;
-
         //Apply potion effects on attacked entity
-        for (PotionEffect potionEffect : PotionUtils.getEffectsFromStack(offhand))
-            victim.addPotionEffect(new PotionEffect(potionEffect.getPotion(), maxDuration, 1));
-
+        for (PotionEffect effect : PotionUtils.getEffectsFromStack(offhand)) {
+            float durationMulti = 0.8F + 0.2F * attacker.getRNG().nextFloat();
+            //TODO: eagle you can vary this multiplier if you really want a random duration. Rn its 80% to 100% of the original duration due to 0.8 + 0.2*[0 to 1]
+            //TODO: i already added the vanilla 1/8 multi for duration so the usual 3mins turn to 22.5 secs
+            // and the usual 8 mins turn to 1 minute
+            // while instant potions stay instant lol
+            //Copied from EntityTippedArrow, duration is downsized by 8 on tipped arrows, added multi
+            victim.addPotionEffect(new PotionEffect(effect.getPotion(), (int) Math.max(effect.getDuration() / 8.F * durationMulti, 1F), effect.getAmplifier(), effect.getIsAmbient(), effect.doesShowParticles()));
+        }
 
         //Swap held tipped arrow for some mobs
         if (attacker.getEntityData().hasKey("NoArrowSwitch")) return;
