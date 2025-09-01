c57e7bc0f1a09f502fddbc58b1055e0c9c3939c9
Nischhelm <nischhelm@gmail.com>
2025-09-01 20:39:39 +0200

    overwrite bad

--- src/main/java/eaglemixins/mixin/reskillable/MixinReskillableBlockOverride.java
+++ src/main/java/eaglemixins/mixin/reskillable/MixinReskillableBlockOverride.java
@@ -1,59 +1,30 @@
 package eaglemixins.mixin.reskillable;
 
 import codersafterdark.reskillable.base.LevelLockHandler;
-import net.minecraft.block.Block;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LevelLockHandler.class)
 public abstract class MixinReskillableBlockOverride {
 
-    /**
-     * Overwrites Reskillable's interaction lock check to bypass for NuclearCraft blocks.
-     */
-    @Overwrite
-    @SubscribeEvent
-    public static void rightClickBlock(PlayerInteractEvent.RightClickBlock event) {
-        BlockPos pos = event.getPos();
-        IBlockState state = event.getWorld().getBlockState(pos);
-        Block block = state.getBlock();
+    //Overwrites Reskillable's interaction lock check to bypass for NuclearCraft blocks.
+    @Inject(
+            method = "rightClickBlock",
+            at = @At("HEAD"),
+            remap = false,
+            cancellable = true
+    )
+    private static void rightClickBlock(PlayerInteractEvent.RightClickBlock event, CallbackInfo ci) {
+        ResourceLocation blockResourceLocation = event.getWorld().getBlockState(event.getPos()).getBlock().getRegistryName();
+        if(blockResourceLocation == null) return;
+        String modid = blockResourceLocation.getNamespace();
 
-        if (block.getRegistryName() != null && (block.getRegistryName().getNamespace().equals("cookingforblockheads") || block.getRegistryName().getNamespace().equals("nuclearcraft"))) {
+        if (modid.equals("cookingforblockheads") || modid.equals("nuclearcraft"))
             // Allow interaction unconditionally
-            return;
-        }
-
-        // Call original logic from Reskillable
-        LevelLockHandler.enforce(event);
-        if (!event.isCanceled()) {
-            if (event.getItemStack().isEmpty()) {
-                EntityPlayer player = event.getEntityPlayer();
-                LevelLockHandler.genericEnforce(event, player, event.getHand().equals(EnumHand.MAIN_HAND) ? player.getHeldItemOffhand() : player.getHeldItemMainhand(), "reskillable.misc.locked.item");
-                if (event.isCanceled()) return;
-            }
-
-            ItemStack stack = new ItemStack(block, 1, block.getMetaFromState(state));
-            if (stack.isEmpty()) {
-                stack = block.getItem(event.getWorld(), pos, state);
-            }
-
-            if (block.hasTileEntity(state)) {
-                TileEntity te = event.getWorld().getTileEntity(pos);
-                if (te != null && !te.isInvalid()) {
-                    stack.setTagCompound(te.writeToNBT(new NBTTagCompound()));
-                }
-            }
-
-            LevelLockHandler.genericEnforce(event, event.getEntityPlayer(), stack, "reskillable.misc.locked.block_use");
-        }
+            ci.cancel();
     }
 }
