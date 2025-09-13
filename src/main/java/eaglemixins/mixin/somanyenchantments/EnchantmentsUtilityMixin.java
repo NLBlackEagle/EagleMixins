fcb299cc6e1fd4f98f0377ca6fd5fb11b1d62278
Nischhelm <nischhelm@gmail.com>
2025-09-13 12:55:02 +0200

    update SME disarming stop to SME 1.0.0+

--- src/main/java/eaglemixins/mixin/somanyenchantments/EnchantmentsUtilityMixin.java
+++ src/main/java/eaglemixins/mixin/somanyenchantments/EnchantmentsUtilityMixin.java
@@ -1,22 +1,22 @@
 package eaglemixins.mixin.somanyenchantments;
 
-import com.Shultrea.Rin.Utility_Sector.EnchantmentsUtility;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.shultrea.rin.enchantments.weapon.EnchantmentDisarmament;
 import eaglemixins.util.DisarmingUtility;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-@Mixin(EnchantmentsUtility.class)
+@Mixin(EnchantmentDisarmament.class)
 public class EnchantmentsUtilityMixin {
     @ModifyArg(
-            method = "Disarm",
+            method = "onLivingAttackEvent",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/EntityLivingBase;entityDropItem(Lnet/minecraft/item/ItemStack;F)Lnet/minecraft/entity/item/EntityItem;")
     )
-    private static ItemStack stopDisarmingGoodStuff(ItemStack stack, @Local(argsOnly = true) EntityLivingBase target) {
-        if (!DisarmingUtility.isAllowedToDisarm(stack, target))
+    private static ItemStack stopDisarmingGoodStuff(ItemStack stack, @Local(argsOnly = true) LivingAttackEvent event) {
+        if (!DisarmingUtility.isAllowedToDisarm(stack, event.getEntityLiving()))
             return ItemStack.EMPTY;
         else
             return stack;
