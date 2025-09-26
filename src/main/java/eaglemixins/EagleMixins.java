b48e27c090856231a7b095e86739d83b45c2f6c6
Nischhelm <nischhelm@gmail.com>
2025-09-26 11:12:30 +0200

    v1.1.5
    hotfix for chests pregenerating loot to check for irradiated items

--- src/main/java/eaglemixins/EagleMixins.java
+++ src/main/java/eaglemixins/EagleMixins.java
@@ -25,7 +25,7 @@ import static eaglemixins.config.ForgeConfigHandler.installFromConfig;
 public class EagleMixins {
 
     public static final String MODID = "eaglemixins";
-    public static final String VERSION = "1.1.4";
+    public static final String VERSION = "1.1.5";
     public static final String NAME = "EagleMixins";
     public static final Logger LOGGER = LogManager.getLogger(NAME);
     public static final SimpleNetworkWrapper NETWORK = NetworkRegistry.INSTANCE.newSimpleChannel(MODID);
