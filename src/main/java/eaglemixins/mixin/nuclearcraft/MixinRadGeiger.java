8a17eba95d62397e2736336f7013999aea1dca72
Nischhelm <nischhelm@gmail.com>
2025-09-29 23:02:24 +0200

    meldexun wins today

--- src/main/java/eaglemixins/mixin/nuclearcraft/MixinRadGeiger.java
+++ src/main/java/eaglemixins/mixin/nuclearcraft/MixinRadGeiger.java
@@ -19,7 +19,7 @@ public abstract class MixinRadGeiger {
 
     @Unique
     private static String eagleMixins$formatRads(double rads, int precision) {
-        int orderOfMagnitude = (int) Math.ceil(Math.log10(Math.abs(rads)));
+        int orderOfMagnitude = (int) Math.floor(Math.log10(Math.abs(rads))) + 1;
         int digitsToUse = MathHelper.clamp(precision - orderOfMagnitude, 0 , precision);
         //use format xx.xx for numbers above 1 and 0.xxxx for numbers below 1 (if n=4)
         return String.format(java.util.Locale.ROOT, "%." + digitsToUse + "f", rads);
