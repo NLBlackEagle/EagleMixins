7787b6d08ade880105cc09fbbb50e93a9fd609f5
Nischhelm <nischhelm@gmail.com>
2025-11-01 17:24:51 +0100

    simplify abyssalgatehandler
    is that even used?

--- src/main/java/eaglemixins/handlers/AbyssalGateHandler.java
+++ src/main/java/eaglemixins/handlers/AbyssalGateHandler.java
@@ -1,18 +1,16 @@
 package eaglemixins.handlers;
 
+import eaglemixins.util.Ref;
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 public class AbyssalGateHandler {
 
@@ -21,21 +19,20 @@ public class AbyssalGateHandler {
 
         EntityPlayer player = event.getEntityPlayer();
         World world = player.world;
-        BlockPos pos = player.getPosition();
 
-        ResourceLocation biomeName = world.getBiome(pos).getRegistryName();
         ItemStack heldItem = player.getHeldItemMainhand();
-        Item quarkRune = ForgeRegistries.ITEMS.getValue(new ResourceLocation("quark", "rune"));
-        Block charmPortal = ForgeRegistries.BLOCKS.getValue(new ResourceLocation("charm", "rune_portal_frame"));
-        Block clickedBlock = world.getBlockState(event.getPos()).getBlock();
-
-        if (charmPortal == null) return;
-        if (biomeName == null) return;
         if (heldItem.isEmpty()) return;
-        if (quarkRune == null) return;
+
+        Item quarkRune = Item.getByNameOrId("quark:rune");
+        if (quarkRune == null || heldItem.getItem() != quarkRune) return;
+
+        Block charmPortal = Block.getBlockFromName("charm:rune_portal_frame");
+        if (charmPortal == null) return;
+
+        Block clickedBlock = world.getBlockState(event.getPos()).getBlock();
         if (clickedBlock != charmPortal) return;
 
-        if (heldItem.getItem() == quarkRune && biomeName.toString().equals("openterraingenerator:overworld_abyssal_gate")) {
+        if (Ref.entityIsInAbyssalGate(player)) {
             player.sendStatusMessage(new TextComponentTranslation("eaglemixins.message.abyssal_runes"), true);
             event.setCanceled(true);
         }
@@ -43,17 +40,11 @@ public class AbyssalGateHandler {
 
     @SubscribeEvent
     public static void onBlockBreak(BlockEvent.BreakEvent event) {
-
         EntityPlayer player = event.getPlayer();
-        World world = event.getWorld();
-        BlockPos pos = event.getPos();
-
         if (player == null) return;
-        if (!(event.getPos().getY() <= 10)) return;
+        if (event.getPos().getY() > 10) return;
 
-        ResourceLocation biomeName = world.getBiome(pos).getRegistryName();
-
-        if (biomeName != null && biomeName.toString().equals("openterraingenerator:overworld_abyssal_gate")) {
+        if (Ref.entityIsInAbyssalGate(player)) {
             player.sendStatusMessage(new TextComponentTranslation("eaglemixins.message.block_break_denied"), true);
             event.setCanceled(true);
         }
